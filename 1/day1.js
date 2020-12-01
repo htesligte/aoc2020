02efebd2b7c44395cbebd2b1f1f971eea3f1916a
Henk te Sligte <henk.tesligte@drukwerkdeal.nl>
2020-12-01 09:07:04 +0100

    day 1

---
+++ 1/day1.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+const data = fs.readFileSync('data.txt');
+const array = data.toString().split("\n");
+
+for (x = 0; x < array.length; x++) {
+    for (y = x+1; y < array.length; y++) {
+        if (parseInt(array[x]) + parseInt(array[y]) === 2020) {
+            console.log(array[x], array[y]);
+            console.log(array[x] * array[y]);
+        }
+    }
+}
